3c98c3221decd2b884953565bc999003f7bff204
Cynthia Dulymbois-Louison <cynthia.d.l@outlook.fr>
2023-12-11 21:01:48 +0100

    2023 Day 11 part 1 (feat R. Barbé)

--- src/main/java/y2023/Day11.java
+++ src/main/java/y2023/Day11.java
@@ -2,13 +2,14 @@ package y2023;
 
 import all.Utils;
 
+import java.awt.Point;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
-record Galaxy(ArrayList<String> map){
-    static Galaxy fromLines(List<String> map){
-        return new Galaxy(new ArrayList<String>(List.copyOf(map)));
+
+record Universe(ArrayList<String> map){
+    static Universe fromLines(List<String> map){
+        return new Universe(new ArrayList<String>(List.copyOf(map)));
     }
 
     private boolean columnIsEmpty(int column){
@@ -39,32 +40,56 @@ record Galaxy(ArrayList<String> map){
             }
         }
     }
+    long plusCourtChemin(Point a, Point b){
+        return Math.abs(b.x - a.x) + Math.abs(b.y - a.y);
+    }
+
+    List<Point> galaxies(){
+        var list = new ArrayList<Point>();
+
+        for (int i = 0; i < map.size(); i++){
+            for (int j = 0; j < map.get(i).length(); j++){
+                if(map.get(i).charAt(j) =='#'){
+                    list.add(new Point(i, j));
+                }
+            }
+        }
+
+        return list;
+    }
 }
 
 public class Day11 {
     public static void main(String[] args) {
-        var lines = Utils.listFromFile("inputs/demo.txt");
+        //var lines = Utils.listFromFile("inputs/demo.txt");
+        var lines = Utils.listFromFile("inputs/y2023/day11_input.txt");
+
+        var universe = Universe.fromLines(lines);
+        universe.expends();
 
 
-
-        var galaxy = Galaxy.fromLines(lines);
-        galaxy.expends();
-
-
-        for (int i = 0; i < galaxy.map().size(); i++) {
+        for (int i = 0; i < universe.map().size(); i++) {
             var sb = new StringBuilder();
 
-            sb.append(galaxy.map().get(i));
+            sb.append(universe.map().get(i));
             sb.append(" * ");
             if(i < lines.size())
                 sb.append(lines.get(i));
 
-            System.out.println(sb.toString());
+            System.out.println(sb);
         }
 
+        var galaxies = universe.galaxies();
 
+        long sum = 0;
 
+        for (int i = 0; i < galaxies.size(); i++){
+            for (int j = i+1; j < galaxies.size(); j++){
+                sum+= universe.plusCourtChemin(galaxies.get(i), galaxies.get(j));
+            }
+        }
 
+        System.out.println("sum = " + sum);
 
     }
 }
