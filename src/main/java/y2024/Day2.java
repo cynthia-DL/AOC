6e69e7dcee1baf4432ff9faa11159447ce3ab122
Cynthia Dulymbois-Louison <cynthia.d.l@outlook.fr>
2024-12-02 13:46:10 +0100

    2024 day 2 part 1

---
+++ src/main/java/y2024/Day2.java
@@ -0,0 +1,77 @@
+package y2024;
+
+import all.Utils;
+
+import java.util.Arrays;
+
+public class Day2 {
+
+    private static boolean isStepIncorrect(long l1, long l2){
+        var  difference = l2 - l1;
+        return difference != 1 && difference != 2 && difference != 3;
+    }
+
+    private static boolean isStepOfMost3(long[] levels){
+        if(isIncreasing(levels)){
+            for (int i = 1; i < levels.length; i++){
+                if (isStepIncorrect(levels[i - 1], levels[i])){
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        for (int i = levels.length - 1; i > 0; i--){
+            if (isStepIncorrect(levels[i], levels[i - 1])){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private  static  boolean isIncreasing(long[] levels){
+        var sorted = Arrays.stream(levels).sorted().toArray();
+
+        return Arrays.equals(sorted, levels);
+    }
+
+    private  static  boolean isDecreasing(long[] levels){
+        for (int i = 1; i < levels.length; i++){
+            if (levels[i-1] <= levels[i]){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static boolean isSafe(long[] levels){
+        return ((isIncreasing(levels) || isDecreasing(levels)) && isStepOfMost3(levels));
+    }
+
+    public static void main(String[] args) {
+        //var input =  Utils.listFromDemoFile();
+        var input =  Utils.listForDay(2024, 2);
+        long res1 = 0L;
+        
+        for (var report : input){
+            var reportArray = Arrays.stream(report.split(" "))
+                    .mapToLong(Long::valueOf)
+                    .toArray();
+
+            System.out.println("______ "+ report +" ______");
+            System.out.println("reportArray = " + Arrays.toString(reportArray));
+            System.out.println("isIncreasing(reportArray) = " + isIncreasing(reportArray));
+            System.out.println("isDecreasing(reportArray) = " + isDecreasing(reportArray));
+            System.out.println("isStepOfMost3(reportArray) = " + isStepOfMost3(reportArray));
+            System.out.println("isSafe(reportArray) = " + isSafe(reportArray));
+            System.out.println("__________________________________");
+
+            if (isSafe(reportArray)){
+                res1++;
+            }
+        }
+
+        System.out.println("res1 = " + res1);
+
+    }
+}
