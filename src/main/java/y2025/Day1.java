2aee89802cdc77340969689cfbd9b989f19f50fc
Cynthia DULYMBOIS-LOUISON <cynthia.d.l@outlook.fr>
2025-12-14 00:14:04 +0100

    day 1 part 2 ca marche sur la demo mais pas sur l'input final envie de canner un peu

--- src/main/java/y2025/Day1.java
+++ src/main/java/y2025/Day1.java
@@ -6,31 +6,49 @@ import java.math.BigInteger;
 import java.util.List;
 
 public class Day1 {
-    static BigInteger getNumToAdd(String mouvement){
+    private static final BigInteger HUNDRED = new BigInteger("100");
+    private static final BigInteger FIFTY = new BigInteger("50");
+
+    private static BigInteger getNumToAdd(String mouvement){
         char sign = mouvement.charAt(0);
         BigInteger unsignNum = new BigInteger(mouvement.substring(1));
 
         return sign == 'L' ? unsignNum.negate() : unsignNum;
     }
-
-    public static BigInteger move(BigInteger from, String mouvement){
-       return from.add(getNumToAdd(mouvement)).mod(new BigInteger("100"));
-    }
-
-    public static void main(String[] args) {
-        //List<String> inputs =  Utils.listFromDemoFile();
-        List<String> inputs =  Utils.listForDay(2025, 1);
-
-        BigInteger startNumber = new BigInteger("50");
+    
+    public static BigInteger process1(List<String> inputs){
+        BigInteger dial = FIFTY;
         BigInteger compteur = BigInteger.ZERO;
         for (var input : inputs){
-            startNumber = move(startNumber, input);
+            dial = dial.add(getNumToAdd(input)).mod(HUNDRED);
 
-            if(startNumber.compareTo(BigInteger.ZERO) == 0){
+            if(dial.compareTo(BigInteger.ZERO) == 0){
                 compteur = compteur.add(BigInteger.ONE);
             }
         }
+        return compteur;
+    }
 
-        System.out.println("compteur = " + compteur);
+    public static BigInteger process2(List<String> inputs){
+        BigInteger dial = FIFTY;
+        BigInteger compteur = BigInteger.ZERO;
+        for (var input : inputs){
+            var toAdd = getNumToAdd(input);
+            var unmod = dial.add(toAdd.abs()).divide(HUNDRED);
+            dial = dial.add(toAdd).mod(HUNDRED);
+
+            if(dial.compareTo(BigInteger.ZERO) == 0 || unmod.compareTo(BigInteger.ZERO) > 0){
+                compteur = compteur.add(BigInteger.ONE.max(unmod));
+            }
+        }
+        return compteur;
+    }
+
+    public static void main(String[] args) {
+        List<String> inputs =  Utils.listFromDemoFile();
+        //List<String> inputs =  Utils.listForDay(2025, 1);
+
+        System.out.println("process1(inputs) = " + process1(inputs));
+        System.out.println("process2(inputs) = " + process2(inputs));
     }
 }
